ed3c8253f8db6042312f6cc3055d0a04cd823079
Nicolás Sañudo <hola@neecosanudo.com>
2022-02-28 20:18:10 -0300

    🎨 Improve format of the code: npm run prettier.

--- utils/tailwindcss.ts
+++ utils/tailwindcss.ts
@@ -1,19 +1,19 @@
 interface TwStyle {
-    dark?: string; // Cuando se aplique el tema oscuro.
-    responsive?: string; // Modificar CSS según el viewport.
-    text?: string; // Todo lo relacionado con tipografía.
-    multimedia?: string; // Imagenes, Videos, SVG, etc.
-    layout?: string; // Grid, Flexbox, Position, Display, etc.
-    box?: string; // Margin, Padding, Box Sizing, Width, Height, etc.
-    accessibility?: string;
-    table?: string;
-    movement?: string; // Transform, Translate, Animation
-  
-    // ¿Cómo llamo a todo esto?
-    effect?: string;
-    filter?: string;
-    interactivity?: string;
-  }
+  dark?: string; // Cuando se aplique el tema oscuro.
+  responsive?: string; // Modificar CSS según el viewport.
+  text?: string; // Todo lo relacionado con tipografía.
+  multimedia?: string; // Imagenes, Videos, SVG, etc.
+  layout?: string; // Grid, Flexbox, Position, Display, etc.
+  box?: string; // Margin, Padding, Box Sizing, Width, Height, etc.
+  accessibility?: string;
+  table?: string;
+  movement?: string; // Transform, Translate, Animation
+
+  // ¿Cómo llamo a todo esto?
+  effect?: string;
+  filter?: string;
+  interactivity?: string;
+}
 
 const TwAssemble = (style: TwStyle): string => {
   let myStyle: string = "";
@@ -27,9 +27,5 @@ const TwAssemble = (style: TwStyle): string => {
   return myStyle;
 };
 
-export {
-    TwAssemble,
-}
-export type {
-    TwStyle,
-}
+export { TwAssemble };
+export type { TwStyle };
