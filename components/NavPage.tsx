a95e4bc25c860730d1e6dcf5787b9780491e3a84
neecosanudo <codigo@neecosanudo.com>
2022-03-04 16:13:25 -0300

    ⚡️ Change internal props handling.
    Content rendering is handled internally by the component instead of
    being passed inside children.

--- components/NavPage.tsx
+++ components/NavPage.tsx
@@ -1,14 +1,41 @@
-import React from "react";
-import {TwStyle, TwAssemble} from '@utils/tailwindcss';
+import { useState } from "react";
+import IconNavPage from "@components/IconNavPage";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
-  children: React.ReactNode;
-  style?: TwStyle,
+  style: string;
+  pages: string[][];
 };
 
-const NavPage = ({ children, style }: Props) => {
-  console.log(style && TwAssemble(style));
-  return <nav className={style && TwAssemble(style)}>{children}</nav>;
+const NavPage = ({ pages, style }: Props) => {
+  const [isClose, setIsClose] = useState(true);
+  const onSetIsClose = () => {
+    setIsClose(!isClose);
+  };
+
+  return (
+    <>
+      <button className={`md:hidden`} onClick={onSetIsClose}>
+        <IconNavPage isClose={isClose} />
+      </button>
+      <nav
+        className={`${
+          isClose
+            ? `hidden md:block ${style}`
+            : `absolute md:static grid md:flex ${style}`
+        }`}
+      >
+        {pages.map((page, i) => {
+          return (
+            <Link key={i} href={page[0]}>
+              <a>{page[1]}</a>
+            </Link>
+          );
+        })}
+      </nav>
+    </>
+  );
 };
 
 export default NavPage;
