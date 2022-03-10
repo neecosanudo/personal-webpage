3ee6a8bf01a957c46ab0b2891095a57cc783e50e
Nicolás Sañudo <hola@neecosanudo.com>
2022-03-10 12:29:22 -0300

    🔨 Update Header component: add new NavPage component, remove old code.

--- widgets/header.tsx
+++ widgets/header.tsx
@@ -1,27 +1,11 @@
 import NavPage from "@components/NavPage";
-import { TwStyle, TwAssemble } from "@utils/tailwindcss";
 
 const Header = () => {
   return (
     <header>
-      <NavPage style={TwAssemble(NavPageStyle)} pages={listOfPages} />
+      <NavPage />
     </header>
   );
-};
-
-const listOfPages = [
-  ["/", "Inicio"],
-  ["/posts", "Blog"],
-  ["/proyectos", "Proyecto"],
-  ["/sobre-mi", "Sobre mí"],
-  ["/contacto", "Contacto"],
-];
-
-const NavPageStyle: TwStyle = {
-  layout: "justify-around",
-  box: "bg-sky-500 w-screen h-screen",
-  text: "text-2xl",
-  responsive: "md:bg-inherit md:w-auto md:h-auto",
 };
 
 export default Header;
